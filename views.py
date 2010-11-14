3290656c63f152dd565ce80134dfe868d189e15c
Joel Burget <joelburget@gmail.com>
2010-11-14 16:36:01 -0500

    Update site in preparation for more views.
    Made posts list marginally nicer, changed google analytics tag.

--- views.py
+++ views.py
@@ -2,5 +2,5 @@ from website.posts.models import Post
 from django.shortcuts import render_to_response
 
 def homepage(request):
-    latest_post_list = Post.objects.all()[:5]
+    latest_post_list = Post.objects.filter(public=True)[:5]
     return render_to_response('homepage.html', {'latest_post_list': latest_post_list})
